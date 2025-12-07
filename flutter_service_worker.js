f7e1892ab52453258d72815ac5f94a04cd1d1941
Patrick Bartholo <patrick.df68@gmail.com>
2025-12-07 14:38:18 -0300

    up 4 - 07/12/2025

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "90e83fc15357fdc91f049bd7055385a6
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "652f1261072b336d8ce431f52e63d7d7",
+"flutter_bootstrap.js": "f6723239f600e60ad5592db8deb22f20",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
